22aa0c367a6f7b3d5bf56c0e4d1b7fd21dd7e2c3
unknown <Victor Silva>
2026-03-04 16:48:07 -0300

    Obtendo um usuario por id

--- src/main/java/com/victorsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/victorsilva/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.victorsilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victorsilva.workshopmongo.domain.User;
 import com.victorsilva.workshopmongo.repositories.UserRepository;
+import com.victorsilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,12 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findById(String id) {
+		User user = repository.findById(id).orElse(null);
+		if( user == null ) {
+			throw new ObjectNotFoundException("Objeto não encotrado");
+		}
+		return user;
+	}
 
 }
