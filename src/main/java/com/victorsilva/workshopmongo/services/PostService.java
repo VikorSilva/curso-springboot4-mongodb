64abe9ec0b54fbdd65f7306acb84517fa64ea157
unknown <Victor Silva>
2026-03-08 14:05:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/victorsilva/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.victorsilva.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorsilva.workshopmongo.domain.Post;
+import com.victorsilva.workshopmongo.repositories.PostRepository;
+import com.victorsilva.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		Post post = repository.findById(id).orElse(null);
+		if( post == null ) {
+			throw new ObjectNotFoundException("Objeto não encotrado");
+		}
+		return post;
+	}
+
+}
